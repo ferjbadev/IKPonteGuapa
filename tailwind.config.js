dc8073f7f1015d14a1b0ef6744e510e1de800b96
Fernando Barrera <barrerafernando54@gmail.com>
2025-10-28 00:35:24 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'fucsia': '#FF1493',
+        'fucsia-light': '#FF69B4',
+        'fucsia-dark': '#C71585',
+      },
+      fontFamily: {
+        'serif': ['Playfair Display', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
